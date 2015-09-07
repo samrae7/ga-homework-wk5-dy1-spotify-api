6ac97ad31362751b357b6d2cd7256cb21a81329a
Sam Rae <samuelrae@gmail.com>
2015-09-07 19:21:52 +0100

    first commit.got basic functionality, API call changes depending on input text and search type selected

---
+++ Spotify_readme_file.txt
@@ -0,0 +1,16 @@
+# Spotify.me
+
+Use Ajax to load data from the Spotify API! A user should be able to:
+
+**Basic Requirements:**  
+
+ - Enter a keyword to seach for.
+ - Select "album", "artist", or "track" as the search type.
+ - Have the names of all search results print as a list.
+ - When the user changes the search type selector, reload the results list based on the new search type.
+ 
+**Keep going:**
+
+ - Add album art!
+ - Add a preview of the song!
+ - Use jQuery to fade the input in/out.
---
+++ js/spotify_me.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+console.log('hello')
+
+var selectCriteria = $('#search-criteria')
+
+var searchBox =$('#input');
+
+var searchString;
+
+var searchType;
+
+selectCriteria.on('change',getResults);
+searchBox.on('change', getSearchString);
+
+function getSearchString() {
+  searchString = searchBox.val().toLowerCase();
+  console.log(searchString);
+}
+
+function getSearchType() {
+  searchType = selectCriteria.val();
+  console.log('Search type =' + searchType)
+}
+
+function getResults() {
+
+  $('#results').empty();
+
+  getSearchType();  
+
+  $.get('https://api.spotify.com/v1/search?q='+searchString+'&type='+searchType, function(response){
+
+  var items =  response[searchType+'s']['items']
+
+  console.log(items);
+
+  $.each(items, function(index, item) {
+    console.log(items[index]['name']);
+
+    $('#results').append("<p>"+items[index]['name']+"</p>")
+  })
+  // $.each(response[searchType+'s']['items'];
+
+  })
+
+
+
+}
+
+
+});
